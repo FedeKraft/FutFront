6cbb2f77e7eca78fb7d2128dccfd2d8b4a30ef48
Federico <fkraft@mail.austral.edu.ar>
2024-04-14 15:41:19 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Login from './Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Login /> {/* Renderiza el componente de inicio de sesión */}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
