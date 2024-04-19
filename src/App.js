f3c632a48313daf8605736af75daa3049d7c00f1
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2024-04-19 01:15:10 -0300

    cooking front

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import './App.css';
 import Register from './register';
 import Login from './login';
+import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
 
+function Auth() {
+    const token = localStorage.getItem('token');
+    if (token) {
+        const {exp} = jwtDecode(token);
+        const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+        if (exp > currentTime) {
+            return <Outlet/>
+        }
+    }
+    return <Navigate to={"/login"}/>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <Register /> {/* Renderiza el componente de Register */}
-          <Login /> {/* Renderiza el componente de Register */}
-
-
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route element={<Auth/>} >
+                    <Route path={"/home"} element={<div>kkLog</div>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
+
+export default App;
+import './App.css';
+import Register from './register';
+import Login from './login';
+import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
+
+function Auth() {
+    const token = localStorage.getItem('token');
+    if (token) {
+        const {exp} = jwtDecode(token);
+        const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+        if (exp > currentTime) {
+            return <Outlet/>
+        }
+    }
+    return <Navigate to={"/login"}/>
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route element={<Auth/>} >
+                    <Route path={"/home"} element={<div>kkLog</div>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
 
 export default App;
