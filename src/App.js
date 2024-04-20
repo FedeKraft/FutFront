5dbf8d76dfeaff5e873d396d9654499467efc1fc
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2024-04-20 16:26:08 -0300

    cooking front

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Register from './register';
-import Login from './login';
+import Register from './pages/register/register';
+import Login from './pages/login/login';
 import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
 import {jwtDecode} from "jwt-decode";
 
@@ -29,40 +29,4 @@ function App() {
         </BrowserRouter>
     );
 }
-
-
-export default App;
-import './App.css';
-import Register from './register';
-import Login from './login';
-import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
-import {jwtDecode} from "jwt-decode";
-
-function Auth() {
-    const token = localStorage.getItem('token');
-    if (token) {
-        const {exp} = jwtDecode(token);
-        const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
-        if (exp > currentTime) {
-            return <Outlet/>
-        }
-    }
-    return <Navigate to={"/login"}/>
-}
-
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/register" element={<Register/>}/>
-                <Route element={<Auth/>} >
-                    <Route path={"/home"} element={<div>kkLog</div>}/>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
-}
-
-
 export default App;
